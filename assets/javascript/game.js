badf73dd392125bcd675c8fcdfd31c5493cbdf5d
jess <jlfinlayson4@gmail.com>
2019-07-10 17:16:11 -0400

    "initial buttons created"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,26 @@
+//current list of nature topics
 var topics = ["clouds", "sunrise", "stars", "dessert", "snow", "moon", "rainbow", "lava", "sand", "waves"]
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + nature + "&api_key=8i9AR4tQiej44GFTMuBNw6Na6HHeFYHA&limit=10";
+//giphy url with topic and api key
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=8i9AR4tQiej44GFTMuBNw6Na6HHeFYHA&limit=10";
 
+//calls url to retrieve gifs
 $.ajax({
     url: queryURL,
     method: "GET"
 })
 
+//function to create buttons for each topic
+function renderButtons() {
+    $("#buttons-view").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var btn = $("<button>");
+        btn.addClass("nature");
+        btn.attr("data-name", topics[i]);
+        btn.text(topics[i]);
+        $("#buttons-view").append(btn);
+    }
+}
+
+//running function to create buttons
+renderButtons();
