65faab96ea602bb633d5819f8b1d1a18976bcf68
jess <jlfinlayson4@gmail.com>
2019-07-10 17:07:01 -0400

    "giphy api linked"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,9 @@
 var topics = ["clouds", "sunrise", "stars", "dessert", "snow", "moon", "rainbow", "lava", "sand", "waves"]
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + nature + "&api_key=8i9AR4tQiej44GFTMuBNw6Na6HHeFYHA&limit=10";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+
