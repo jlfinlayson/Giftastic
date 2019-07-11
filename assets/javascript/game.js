7c0690b46e9607650ba73a235c739897c4f8c451
jess <jlfinlayson4@gmail.com>
2019-07-11 19:22:55 -0400

    "gifs animate!"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,24 +38,36 @@ $(".natureElement").on("click", function () {
         url: queryURL,
         method: "GET",
     })
-
-    
-        //retrieves gif results and puts them in html
+//retrieves gif results and puts them in html
         .then(function (response) {
             var results = response.data;
             for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div>");
                 gifDiv.addClass("gif")
                 var rating = results[i].rating;
-                var p = $("<p>").text("Rating: " + rating);
+                var p = $("<p>").text("rating: " + rating);
                 var natureImage = $("<img>");
-                natureImage.attr("data-state", "")
+                natureImage.addClass =("natureImage")
+                natureImage.attr("id", "natImage");
+                // natureImage.attr("data-state", "still")
                 natureImage.attr("src", results[i].images.fixed_height_still.url);
+            natureImage.attr({'data-animate' : results[i].images.fixed_height.url});
+            natureImage.attr({'data-state' : "still"});
+            natureImage.attr({'data-still' : results[i].images.fixed_height_still.url});
                 gifDiv.append(natureImage);
                 gifDiv.append(p);
                 $("#gifs-appear-here").prepend(gifDiv);
             }
         })
-
 })
 })
+$(document).on("click", "img", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  })
